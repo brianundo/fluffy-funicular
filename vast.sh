856456875a32aa914316e298290c8bfcdac1358f
Undo the Brian <febro.aw20@gmail.com>
2026-05-22 13:23:02 +0700

    Add files via upload

---
+++ vast.sh
@@ -0,0 +1,8 @@
+## DOWNLOAD MINER
+curl -L -o alpha-miner https://pearl.alphapool.tech/downloads/alpha-miner
+
+## GIVE CHMOD
+chmod +x alpha-miner
+
+## RUN MINER
+./alpha-miner --pool stratum+tcp://us2.alphapool.tech:5566 --address prl1pt27f6j9282zf6gvwcx66q9pemkhyt55wzu5c0ff38x6nm4gsgd4q3wsr9k
