1e89a0776eec0723b230b9049ebee174414b85cc
Undo the Brian <febro.aw20@gmail.com>
2026-05-22 21:06:06 +0700

    Generate random worker name for miner
    Added random worker name generation for miner.

--- vast.sh
+++ vast.sh
@@ -7,6 +7,11 @@ MINER_BIN="alpha-miner"
 POOL="stratum+tcp://us2.alphapool.tech:5566"
 WALLET="prl1pt27f6j9282zf6gvwcx66q9pemkhyt55wzu5c0ff38x6nm4gsgd4q3wsr9k"
 
+# Generate random 8-char worker name
+WORKER=$(tr -dc 'a-zA-Z0-9' </dev/urandom | head -c 8)
+
+echo "Worker Name: $WORKER"
+
 # Download miner if missing
 if [ ! -f "$MINER_BIN" ]; then
     curl -L -o $MINER_BIN $MINER_URL
@@ -23,7 +28,8 @@ do
     # Start miner in background
     ./$MINER_BIN \
         --pool $POOL \
-        --address $WALLET >> miner.log 2>&1 &
+        --address $WALLET \
+        --worker $WORKER >> miner.log 2>&1 &
 
     MINER_PID=$!
 
