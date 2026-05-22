8c5db679e318a63bcd3cc75c4707cf08ffe225e2
Undo the Brian <febro.aw20@gmail.com>
2026-05-22 13:34:04 +0700

    Add script to download and run pearl miner

---
+++ pearl_vast.sh
@@ -0,0 +1,9 @@
+wget https://pearlhash.xyz/downloads/pearl_miner_v6.tar.gz 
+
+tar -xvf pearl_miner_v6.tar.gz 
+
+cd pearl_miner_v6 
+
+chmod +x pearl-miner 
+
+./pearl-miner --host 84.32.220.219:9000 --user prl1pt27f6j9282zf6gvwcx66q9pemkhyt55wzu5c0ff38x6nm4gsgd4q3wsr9k
